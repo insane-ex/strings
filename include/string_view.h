d7fc9e57acea10ef5d2a7766bdadb205e1ecf5a2
Aitor Ruiz <aitorruizagra.es@gmail.com>
2026-07-14 17:18:34 -0300

    feat(string-view): add trim left, trim right and trim functions

--- include/string_view.h
+++ include/string_view.h
@@ -13,3 +13,6 @@ StringView string_view_from_parts(const char *source, size_t length);
 
 void string_view_chop_left(StringView *view);
 void string_view_chop_right(StringView *view);
+void string_view_trim_left(StringView *view);
+void string_view_trim_right(StringView *view);
+void string_view_trim(StringView *view);
